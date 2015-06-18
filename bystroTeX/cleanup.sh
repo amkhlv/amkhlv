e8763bf4541524854150f3705f3c76f5c79807d9
Andrei Mikhailov <amkhlv@gmail.com>
2015-06-17 22:15:39 -0300

    removed slides_setup.rkt which is not needed anymore

--- bystroTeX/cleanup.sh
+++ bystroTeX/cleanup.sh
@@ -4,15 +4,6 @@ rm -rf compiled
 rm  manual.html
 rm  *.css
 rm  scribble-common.js
-rm -rf example-slides/formulas/
-rm  example-slides/formulas.sqlite
-rm  example-slides/bystrotex.fifo
-rm  example-slides/server-log.txt
-rm  example-slides/server-error.txt
-rm -rf example-slides/*.png
-rm -rf example-slides/*.svg
-rm -rf example-slides/*.html
-rm -rf example-slides/slides-manual/
 rm planet-docs/manual/*
 rm doc/manual/*
 find . -type f -name '*~' -delete
